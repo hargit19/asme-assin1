ae8ca5addb5e82a917df65d7b31b7c61846908fb
hargit19 <127594594+hargit19@users.noreply.github.com>
2023-07-12 01:09:23 +0530

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,14 @@
+function myfunction() {
+   if( document.getElementsByClassName("navbar")[0].style.right == "100%"){
+    document.getElementsByClassName("navbar")[0].style.right = "80%";
+   }
+else{document.getElementsByClassName("navbar")[0].style.right ="100%" ;}
+}
+function myfunction2() {
+    if(document.getElementsByClassName("navbar")[0].style.right == "100%"){
+        document.getElementsByClassName("navbar")[0].style.right = "0";
+    }
+    else{
+        document.getElementsByClassName("navbar")[0].style.right = "100%";
+    }
+}
